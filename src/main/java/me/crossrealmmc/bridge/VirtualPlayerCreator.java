3cc12189cc03c041c85f50dc0c4a0713c336a588
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-25 17:30:38 -0600

    Create VirtualPlayerCreator.java

---
+++ src/main/java/me/crossrealmmc/bridge/VirtualPlayerCreator.java
@@ -0,0 +1,46 @@
+package me.crossrealmmc.bridge;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_21_R3.CraftServer;
+import org.bukkit.entity.Player;
+
+import java.net.InetSocketAddress;
+import java.util.UUID;
+
+public class VirtualPlayerCreator {
+
+    public static Player createVirtualPlayer(UUID uuid, String username, InetSocketAddress address) {
+        try {
+            CraftServer server = (CraftServer) Bukkit.getServer();
+            MinecraftServer nmsServer = server.getServer();
+            
+            GameProfile profile = new GameProfile(uuid, username);
+            
+            ServerPlayer serverPlayer = new ServerPlayer(
+                nmsServer,
+                nmsServer.overworld(),
+                profile
+            );
+            
+            // Crear conexión virtual
+            serverPlayer.connection = new ServerGamePacketListenerImpl(
+                nmsServer,
+                null,
+                serverPlayer
+            );
+            
+            // Añadir al servidor
+            server.addPlayer(serverPlayer.getBukkitEntity());
+            
+            return serverPlayer.getBukkitEntity();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
